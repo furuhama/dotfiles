b2b477fca4d1887c2880c1c40eb759a434061d6a
furuhama <furuhama@users.noreply.github.com>
2018-02-04 11:14:02 +0900

    Add autofetch.sh

---
+++ autofetch.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# run `git fetch origin` for all directories
+# in this folder (except for this shell script itself)
+
+# function to run `git fetch` for a directory
+function fetch_origin () {
+  echo "fetching $1..."
+  cd $1
+  git fetch origin
+  echo 'end'
+  echo ''
+  cd ..
+}
+
+# function to run fetch_origin for every directory
+function fetch_all () {
+  echo 'auto fetching start...'
+
+  echo ''
+
+  # iterate for every directory name
+  # MEMO: "${0#*/}" means this shell script file name itself
+  for dir_name in `ls | grep -v "${0#*/}"`
+  do
+    fetch_origin $dir_name
+  done
+
+  echo 'All repositories would be up to date!'
+}
+
+# main function
+fetch_all
+
