a5377bec2dff54b554d7fb210df0808cf513f637
furuhama <furuhama@users.noreply.github.com>
2018-09-24 13:09:42 +0900

    Improve pulling_all.sh

--- shell_scripts/pulling_all.sh
+++ shell_scripts/pulling_all.sh
@@ -1,27 +1,41 @@
 #!/usr/bin/env sh
 
-git_repos=( \
-  "9cc master" \
-  "codesearch master" \
-  "gauche master" \
-  "gitlab-ce master" \
-  "golang master" \
-  "gvisor master" \
-  "lld master" \
-  "mastodon master" \
-  "openal-soft master" \
-  "openssl master" \
-  "rails master" \
-  "regex master" \
-  "ruby trunk" \
-  "rust master" \
-  "servo master" \
-  "tor master" \
-)
+# `pulling_all.sh` is an automaric git pulling processer.
+# This would improve your reading git repositories to pull upstream changes.
+#
+#
+# USAGE:
+# Just run `./pulling_all.sh` in your directory.
+# To be sure to put `git_repos.sh` & `svn_repos.sh` files at the same directory.
+#
+#
+# EXAMPLE:
+#
+# == git_repos.sh ==
+#
+# git_repos=( \
+#   "[repository_name] [branch_name]" \
+#   "[repository_name] [branch_name]" \
+#
+#   ...
+#
+# )
+#
+# == svn_repos.sh ==
+#
+# svn_repos=( \
+#   "[repository_name]" \
+#   "[repository_name]" \
+#
+#   ...
+#
+# )
 
-svn_repos=( \
-  "firefox" \
-)
+# get git repositories setting
+source git_repos.sh
+
+# get svn repositories setting
+source svn_repos.sh
 
 # pulling all git repositories
 function git_pull() {
@@ -31,22 +45,32 @@ function git_pull() {
     local repo=${repo_and_branch[0]}
     local branch=${repo_and_branch[1]}
 
-    cd ${repo}
+    echo "==============================================="
     echo "start pulling => ${repo} [${branch}]"
+    echo "-----------------------------------------------\n"
+
+    cd ${repo}
     git pull --rebase origin ${branch}
-    echo "end\n"
     cd ..
+
+    echo "end"
+    echo "===============================================\n"
   done
 }
 
 # pulling all svn repositories
 function svn_pull() {
   for repo in "${svn_repos[@]}"; do
-    cd ${repo[0]}
+    echo "==============================================="
     echo "start pulling => ${repo[0]}"
+    echo "-----------------------------------------------\n"
+
+    cd ${repo[0]}
     hg pull --rebase
-    echo "end\n"
     cd ..
+
+    echo "end"
+    echo "===============================================\n"
   done
 }
 
@@ -55,4 +79,8 @@ function main() {
   git_pull
   svn_pull
 }
-main
+
+# To be sure that `$git_repos` and `$svn_repos` variables are defined.
+if [ -n "$git_repos" ] && [ -n "$svn_repos" ]; then
+  main
+fi
